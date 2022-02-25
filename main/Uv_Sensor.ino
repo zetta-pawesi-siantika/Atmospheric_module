309f5a9df5e62ca6b5e919e2aa8b26a7ab7cc754
I Putu Pawesi Siantika <csiantka@gmail.com>
2022-02-25 10:27:23 +0800

    creating Uv_sensor tab, the code works

--- main/Uv_Sensor.ino
+++ main/Uv_Sensor.ino
@@ -1,1 +1,16 @@
+/* UV sensor properties */
+// it produces value 0 - 1023 (10 bit)
 
+// variables global
+int gUvdata;
+
+// setup method
+void setupUvsensor(){
+  pinMode(UVPIN, INPUT);
+  
+}
+
+void readUvsensor(){
+  gUvdata = analogRead(UVPIN);
+  Serial.println(gUvdata); // debuging: shows value on serial monitor 
+}
--- main/main.ino
+++ main/main.ino
@@ -2,11 +2,13 @@ void setup() {
   Serial.begin(9600); // begin serial communication
   
   setupRainsensor();
+  setupUvsensor();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   readRainsensor();
- 
+  readUvsensor();
+  delay(1000);
 }
