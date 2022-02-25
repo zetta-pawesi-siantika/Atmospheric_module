8dca99fb904111e8b99509ead9014708f4797f00
I Putu Pawesi Siantika <csiantka@gmail.com>
2022-02-25 10:10:38 +0800

    rain sensor code works

--- main/Rain_Sensor.ino
+++ main/Rain_Sensor.ino
@@ -1,1 +1,15 @@
+/* Rain sensor properties */
+// it produces analog value 0 - 1023 (10 bit)
 
+// variables
+int rainData; 
+
+// setup method 
+void setupRainsensor(){
+  pinMode(RAINPIN, INPUT);
+}
+
+void readRainsensor(){
+  rainData =  analogRead(RAINPIN);
+  Serial.println(rainData);
+}
--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // begin serial communication
+  
+  setupRainsensor();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  readRainsensor();
+  delay(1000);
+  
 
 }
