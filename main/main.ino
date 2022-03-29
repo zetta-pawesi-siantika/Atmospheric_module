4584722b12f34cec95eddd05e9da0e5c3ac4d9a0
I Putu Pawesi Siantika <pawesi.siantika@zettabyte.life>
2022-03-29 14:13:34 +0800

    Removing Rain fall sensor

--- main/main.ino
+++ main/main.ino
@@ -9,37 +9,21 @@
 #define VOLTAGE_REF_5V 5.0 // vcc sensor's is 5.0 V
 #define ADC_RESOLUTION 1023.0 // 10-bits resolution
 #define DELAY_TIME 1000
-#define BYTE_LENGTH 10
-
-
-char gRainfallrate_char[BYTE_LENGTH];
-byte i = 0;
-
 
 #include "Data_Capture.h"
 #include "IO_Mapping.h"
 
 // use preprocessor method (check documentation: https://docs.google.com/document/d/10_jPgvdRyReOkWolBOLf4YiwogQpMxXjzttXRmqAFns/edit)
-#define DEBUG_UV
-#define DEBUG_UV_VOLTAGE
+#define DEBUG_ALL
 
 
-//#define SERIAL_COM_AVAILABLE // it has declare this 
+
 
 void setup() {
   pinMode(PIN_TRIGGER_RTC, OUTPUT);
   digitalWrite(PIN_TRIGGER_RTC, LOW); // enable RTC module
 
   Serial.begin(9600); // begin serial communication
-  Serial1.begin(9600);
-
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(1000);
-
-
   setupCom();
   setupRainsensor();
   setupUvsensor();
@@ -48,36 +32,10 @@ void setup() {
   setupWinddirectionsensor();
   setupBme280sensor();
   setupBatterylevel();
-  setupDatalogger();
+   setupDatalogger();
 }
 
 void loop() {
-#ifdef SERIAL_COM_AVAILABLE
-  while (Serial1.available() == 0) {
-    // do nothing when data isn't comming
-  }
-
-  // if something come from serial
-  while (Serial1.available() > 0) {
-     Serial1.readBytes(gRainfallrate_char, BYTE_LENGTH);
-    
-    digitalWrite(LED, HIGH);
-    delay(800);
-    digitalWrite(LED, LOW);
-    delay(800);
-
-  }
-  // convert data in string data type
-  gWaterfallrate_str = String(gRainfallrate_char);
-  Serial.print("Water fall rate : ");
-  Serial.print(gWaterfallrate_str);
-  Serial.print(" mm");
-  
-  // turn on led for 5 secs. ending read data
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-#endif
 
   rainData();
   readUvsensor();
@@ -86,8 +44,8 @@ void loop() {
   readWinddirectionsensor();
   readBme280sensor();
   batteryLevel();
-//  dataLogger();
-//  sendDatatoserver();
+  dataLogger();
+  sendDatatoserver();
   Serial.println();
   delay(DELAY_TIME);
   Serial1.write("DONE", 10);
