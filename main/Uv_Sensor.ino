c6d71090207f27de0e5bde57bccbbb6da4351228
I Putu Pawesi Siantika <pawesi.siantika@zettabyte.life>
2022-04-30 20:33:39 +0800

    cleaned code: UV sensor

--- main/Uv_Sensor.ino
+++ main/Uv_Sensor.ino
@@ -1,34 +1,22 @@
-
-/* UV sensor properties */
-// it produces UV Index value
-
-// variables local
-
-int _readDataADCUV;
-float _voltageReadUV;
-
-// Const for local
-// it was got by calculate the datasheet value ( data is linear )
-const float UV_SLOPE = 0.01;
-const float V_REF_UV = 3300.0; // mV
-
-
-// setup method
+/* This file contains GUVA 12 sensor (UV sensor)
+ *  It measures UV index (0-11)
+ *  src: https://electropeak.com/learn/interfacing-guva-s12sd-uv-light-sensor-module-with-arduino/
+ * 
+ */
+   
 void setupUvsensor() {
   pinMode(UVPIN, INPUT);
 }
 
 
-// converting to UV Index
-// src: https://electropeak.com/learn/interfacing-guva-s12sd-uv-light-sensor-module-with-arduino/
-
 void readUvsensor() {
-  _readDataADCUV = analogRead(UVPIN);
+  int _readDataADCUV;
+  float _voltageReadUV;
 
-#ifdef DEBUG_UV_ADC
-  Serial.print(" UV ADC Value: ");
-  Serial.println(_readDataADCUV );
-#endif
+  PROGMEM const float UV_SLOPE = 0.01;
+  PROGMEM const float V_REF_UV = 3300.0; // mV
+
+  _readDataADCUV = analogRead(UVPIN);
 
   _voltageReadUV = ( _readDataADCUV / ADC_RESOLUTION) * V_REF_UV;
   gUvindex = (UV_SLOPE * _voltageReadUV);
@@ -38,16 +26,8 @@ void readUvsensor() {
     gUvindex = 0.0;
   }
 
-  /* Serial debug */
 #if defined DEBUG_UV || defined DEBUG_ALL
   Serial.print("UV index: ");
   Serial.println(gUvindex);
-
 #endif
-
-#ifdef DEBUG_UV_VOLTAGE
-  Serial.print(" UV Voltage (mV): ");
-  Serial.println(_voltageReadUV);
-#endif
-
 }
