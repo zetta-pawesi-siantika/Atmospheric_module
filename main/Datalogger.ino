31981169391d0ac683fae66bf345ce94eab773bb
I Putu Pawesi Siantika <pawesi.siantika@zettabyte.life>
2022-04-30 21:00:33 +0800

    cleaned code: Data logger

--- main/Datalogger.ino
+++ main/Datalogger.ino
@@ -6,48 +6,33 @@
 #include <SPI.h>
 #include <SD.h>
 
-String FILE_TITLE ="ATM_TEST.txt";
-
-// Local Conts 
-String TEMPLATE_TITLE =" Date,Time Stamp,Rain Intens,Uv Index,Lux,Solar Irradiance(W/m2),PAR(µmoles/m2/s),Wind spd(m/s),Wind Direction,Temp(C),pressure(hPa),Humd(%)"; 
-
 File dataFile;
 
 void setupDatalogger()
 {
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
   pinMode(SS, OUTPUT);
   
   if (!SD.begin(CHIP_SELECT)) {
-    Serial.println("Card failed, or not present");
-   
+    Serial.println(F("Card failed, or not present"));
   }
-  Serial.println("card initialized.");
+  Serial.println(F("card initialized."));
   
 
-  dataFile = SD.open(FILE_TITLE, FILE_WRITE);
+  dataFile = SD.open(F("ATM.txt"), FILE_WRITE);
   if (! dataFile) {
-    Serial.println("error opening" + FILE_TITLE );
+    Serial.println(F("error opening ATM.txt"));
   
   }
   // template for title
-  dataFile.println(TEMPLATE_TITLE);
-  Serial.println(TEMPLATE_TITLE);
+  dataFile.println(F(" Date,Time Stamp,Rain Intens,Uv Index,Lux,Solar Irradiance(W/m2),PAR(µmoles/m2/s),Wind spd(m/s),Wind Direction,Temp(C),pressure(hPa),Humd(%)"));
 }
 
 void dataLogger()
-{
-  // make a string for assembling the data to log:
-  String dataString = "";
-
-
-  dataString = dataString +  dateNow + ","+ timeNow + "," + String(gRaindata)+ "," + String(gUvindex)+ "," + String(gLux)+ "," + String(gIrradiance)+ ","+ String(gPAR)+ "," + String(gWindspeed)+ "," + String(gWinddirectiondata)+ "," + String(gTemperature)+ ","+ String(gPressure)+ ","+ String(gHumidity); 
-  dataFile.println(dataString);
+{ 
+  dataFile.println( dateNow + ","+ timeNow + "," + String(gRaindata)+ "," + String(gUvindex)+ "," + String(gLux)+ "," + String(gIrradiance)+ ","+ String(gPAR)+ "," + String(gWindspeed)+ "," + String(gWinddirectiondata)+ "," + String(gTemperature)+ ","+ String(gPressure)+ ","+ String(gHumidity));
   #if defined DEBUG_LOGGER || defined DEBUG_ALL
-  Serial.println(dataString);
+  Serial.println(F("data Logged"));
   #endif
-  dataFile.flush();\
+  dataFile.flush();
   
 }
